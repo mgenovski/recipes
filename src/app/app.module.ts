b12f76b062249cc88f9d861fee661df1ffd25406
mgenovski <mgenovski@gmail.com>
2022-04-24 23:26:11 +0300

    Add auth routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { PagesModule } from './feature/pages/pages.module';
 import { RecipesModule } from './feature/recipes/recipes.module';
@@ -16,7 +17,8 @@ import { RecipesModule } from './feature/recipes/recipes.module';
     AppRoutingModule,
     CoreModule,
     RecipesModule,
-    PagesModule
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
