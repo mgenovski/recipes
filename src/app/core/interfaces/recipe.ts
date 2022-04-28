c818748ae017c1d32c00fcbaa9b56692503854e7
mgenovski <mgenovski@gmail.com>
2022-04-28 21:12:14 +0300

    Fix ownerId in recipe interface

--- src/app/core/interfaces/recipe.ts
+++ src/app/core/interfaces/recipe.ts
@@ -2,9 +2,14 @@ import { IBase } from './base';
 
 export interface IRecipe<T = string> extends IBase {
     title: string;
-    _owenrId: string;
+    _ownerId: string;
     ownerName: string;
     imgUrl: string;
     description: string;
+    servings: string;
+    time: string;
+    difficulty: string;
+    steps: string[];
+    ingredients: string[];
   }
   
