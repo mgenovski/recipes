7a5936e4ad6ed7775168fda7d9a60f5534f2c36d
mgenovski <mgenovski@gmail.com>
2022-04-28 21:11:36 +0300

    Add edit component

---
+++ src/app/feature/recipes/edit-recipe-page/edit-recipe-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-recipe-page',
+  templateUrl: './edit-recipe-page.component.html',
+  styleUrls: ['./edit-recipe-page.component.css']
+})
+export class EditRecipePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
